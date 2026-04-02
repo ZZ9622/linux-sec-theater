4ce885220a0f1503818e13e6f86727ac8b4f51e4
Maychord <chen001924@gmail.com>
2026-04-02 12:12:03 +0200

    rhel output

--- script /fetch_redhat_pkgs.py
+++ script /fetch_redhat_pkgs.py
@@ -1,24 +1,25 @@
+# from rhel machine 
+# cat /etc/redhat-release
+# Red Hat Enterprise Linux release 10.1 (Coughlan)
+
 import subprocess
 import csv
 
 def get_rhel_count():
-    # 使用 --available 替换 --enabled
-    # 增加 --refresh 确保获取的是最新的元数据
+
     cmd = "dnf repoquery --available --qf '%{name}' -y"
     try:
         output = subprocess.check_output(cmd, shell=True).decode()
-        # splitlines() 会根据换行符切分，set() 自动去重
+        # splitlines() will split the output into lines, and we use a set to ensure uniqueness
         pkgs = set(line.strip() for line in output.splitlines() if line.strip())
         return sorted(list(pkgs))
     except subprocess.CalledProcessError as e:
-        print(f"执行 DNF 命令失败: {e}")
+        print(f"failed to execute DNF command: {e}")
         return []
 
-# 执行并保存
 rhel_list = get_rhel_count()
 
 if rhel_list:
-    # 确保 ../data 目录存在
     import os
     os.makedirs('./data', exist_ok=True)
     with open('./data/rhel_10_pkgs.csv', 'w', newline='', encoding='utf-8') as f:
@@ -28,4 +29,4 @@ if rhel_list:
             writer.writerow([p])
     print(f"RHEL 10.1 Official Packages: {len(rhel_list)}")
 else:
-    print("未能获取到任何软件包。")
+    print("failed to retrieve any packages.")
